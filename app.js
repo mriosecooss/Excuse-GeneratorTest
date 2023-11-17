ff11627aff5212d620ab50a2c1bbe0d859eba971
mriosecooss <mriosecooss@gmail.com>
2023-11-16 23:33:19 -0300

    GifForLinkedIn

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
-/* eslint-disable */
-//import "bootstrap";
-//import "./style.css";
-//
-//import "./assets/img/rigo-baby.jpg";
-//import "./assets/img/4geeks.ico";
-//
-//window.onload = function() {
+let who = ['El perro', 'Mi abuela', 'La Tortuga', 'Mi Ave', 'Mi mama'];
+let action = ['comio', 'orino', 'trituro', 'rompio', 'extravio', 'escondio', 'robo'];
+let what = ['mi tarea', 'las llaves', 'el auto', 'mis cremas', 'mi libro', 'mi notebook'];
+let when = ['antes de la clase', 'justo a tiempo', 'antes de empezar el dia', 'durante el almuerzo', 'mientras almorzaba'];
 
-  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-  let action = ['ate', 'peed', 'crushed', 'broke'];
-  let what = ['my homework', 'the keys', 'the car'];
-  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
-  
-  function excuseGenerator(Who, Action, What, When) {
-    numWho = Math.floor(Math.random() * who.length);
-    numAction = Math.floor(Math.random() * action.length);
-    numWhat = Math.floor(Math.random() * what.length);
-    numWhen = Math.floor(Math.random() * when.length);
-    
-    return Who[numWho] + " " + Action[numAction] + " " + What[numWhat] + " " + When[numWhen] + " "
-  }
-  
-  //function onLoad(){
-  //  var excuse = document.getElementById('excuse');
-  //  excuse.innerHTML = excuseGenerator(who, action, what, when);
-  //}
-  //window.onload = onLoad;
-  //console.log("- - -");
-//};
+function excuseGenerator(Who, Action, What, When) {
+  numWho = Math.floor(Math.random() * who.length);
+  numAction = Math.floor(Math.random() * action.length);
+  numWhat = Math.floor(Math.random() * what.length);
+  numWhen = Math.floor(Math.random() * when.length);
+
+  return Who[numWho] + " " + Action[numAction] + " " + What[numWhat] + " " + When[numWhen] + " "
+}
 
 console.log(excuseGenerator(who, action, what, when));
